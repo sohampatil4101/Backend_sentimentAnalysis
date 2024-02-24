2188eb3aa49cbeca78c3f0c2aaf021548b5214b1
Soham Patil <patilsoham390@gmail.com>
2024-02-24 16:22:51 +0530

    chatbot api done

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home',views.home, name='home')
+    path('home',views.home, name='home'),
+    path('postfaq/',views.PostFaq.as_view()),
+
 ]
